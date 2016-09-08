b95c75d5b27b4ac229dadd7e26e471ff4db4fef4
Guto Carvalho <gutocarvalho@gmail.com>
2016-09-07 21:41:56 -0300

    box update, new puppet version, minor fixes/updates

--- cleanup.sh
+++ cleanup.sh
@@ -15,14 +15,10 @@ find /var/log -type f | while read f; do echo -ne '' > $f; done;
 # remove under tmp directory
 rm -rf /tmp/*
 
-# Make sure Udev doesn't block our network
-echo "cleaning up udev rules"
-rm /etc/udev/rules.d/70-persistent-net.rules
-mkdir /etc/udev/rules.d/70-persistent-net.rules
-rm -rf /dev/.udev/
-rm /lib/udev/rules.d/75-persistent-net-generator.rules
-
-echo "Adding a 2 sec delay to the interface up, to make the dhclient happy"
+# Make eth0 auto dhcp
+echo 'auto eth0' >> /etc/network/interfaces
+echo 'iface eth0 inet dhcp' >> /etc/network/interfaces
+# Adding a 2 sec delay to the interface up, to make the dhclient happy"
 echo "pre-up sleep 2" >> /etc/network/interfaces
 
 dd if=/dev/zero of=/EMPTY bs=1M
